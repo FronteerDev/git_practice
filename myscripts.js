d0c1cd3b25bba75a62b44c4cac2964bc5cbcb707
FronteerDev <98895968+FronteerDev@users.noreply.github.com>
2022-05-08 19:05:53 +0200

    Update with Magic Story

--- myscripts.js
+++ myscripts.js
@@ -15,3 +15,12 @@ function changeText2(){
 	var userInput = document.getElementById('name').value;
 	document.getElementById("demo").innerHTML = "Good Job " + userInput + "!!";
 }
+
+document.getElementById("submit").addEventListener("click", makeStory);
+
+function makeStory() {
+	var favAnimal = document.getElementById("favAnimal").value.toLowerCase();
+	var verb = document.getElementById("verb").value.toLowerCase();
+	var planet = document.getElementById("planet").value.toLowerCase();
+	document.getElementById("story").innerHTML = "On a summerday a " + favAnimal + " was " + verb + " on a beach on " + planet + ".";
+}
